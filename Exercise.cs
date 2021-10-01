2d3d25d100d5cdb2f03bb9c9398fee48e3b4a681
SamuelStrycek <91180378+SamuelStrycek@users.noreply.github.com>
2021-10-01 09:57:49 +0200

    Add files via upload

---
+++ Exercise.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Open_Lab_02._01
+{
+    public class Exercise
+    {
+        public double[] Create5Numbers()
+        {
+            double[] array = {1.5, 3.3, 5.2, 7.9, 9.1};
+            return array;
+        }
+
+        public double GetSecond(double[] numbers)
+        {
+            return numbers[1]
+        }
+    }
+}
